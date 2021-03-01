34faa045fcafa1e8d94cf8e6206590697b595100
hhhallan <allanstm76@gmail.com>
2021-03-01 14:13:29 +0100

    first commit

--- Config/DbConfig.php
+++ Config/DbConfig.php
@@ -1,8 +1,11 @@
 <?php
-
-$config = [
-    "dbName" => "bdd-restaurant",
-    "dbHost" => "localhost",
-    "dbUser" => "root",
-    "dbPassword" => ""
+/**
+ * Indiquez les identifiants de connexion à votre BDD
+ */
+return [
+    'dbName' => 'createSite', // Le nom de la base de données
+    'dbHost' => '127.0.0.1', // Le nom de l'host de la base de données
+    'dbPort' => '8889', // Le numéro du port de la base de données
+    'dbUser' => 'root', // Le nom d'utilisateur qui vous a été attribué
+    'dbPassword' => 'root' // Le password que vous avez choisi lors de la création de votre base de données
 ];
--- public/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-use Core\App;
-
-define("ROOT", dirname(__DIR__)."/");
-require ROOT."Core/App.php";
-App::load();
-
-include ROOT."Config/routeur.php";
