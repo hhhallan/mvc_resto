f966508470e270ee4ed9e5a83da50477a48aa822
hhhallan <allanstm76@gmail.com>
2021-03-01 14:18:20 +0100

    c good

--- Config/DbConfig.php
+++ Config/DbConfig.php
@@ -3,9 +3,8 @@
  * Indiquez les identifiants de connexion à votre BDD
  */
 return [
-    'dbName' => 'createSite', // Le nom de la base de données
-    'dbHost' => '127.0.0.1', // Le nom de l'host de la base de données
-    'dbPort' => '8889', // Le numéro du port de la base de données
+    'dbName' => 'mvc_blog', // Le nom de la base de données
+    'dbHost' => 'localhost', // Le nom de l'host de la base de données
     'dbUser' => 'root', // Le nom d'utilisateur qui vous a été attribué
-    'dbPassword' => 'root' // Le password que vous avez choisi lors de la création de votre base de données
+    'dbPassword' => '' // Le password que vous avez choisi lors de la création de votre base de données
 ];
